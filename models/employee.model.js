08b5a98a8cc358c9954304273d3b559e034c1fd7
sonali-vaidya <31893800+sonali-vaidya@users.noreply.github.com>
2019-01-31 00:32:14 +0530

    Add files via upload

---
+++ models/employee.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+var employeeSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'This field is required.'
+    },
+    EmployeeID: {
+        type: String,
+        required: 'This field is required.'
+    },
+    email: {
+        type: String,
+        required: 'This field is required.'
+    },
+    
+    Mobile: {
+        type: String,
+        required: 'This field is required.'
+    },
+    IMEI: {
+        type: String,
+        required: 'This field is required.'
+    }
+});
+
+// Custom validation for email
+employeeSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+mongoose.model('Employee', employeeSchema);
